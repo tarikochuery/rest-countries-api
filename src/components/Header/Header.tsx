d7603d8de08be2a6fe2750769f0f314d75ccef33
tariko <ttarikechuery@gmail.com>
2022-08-23 05:23:02 -0300

    Add Header/ Theme Changer on

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useContext } from "react"
+import { ThemeContext } from "styled-components"
 import HeaderContainer from "./style"
 
 interface Props {
@@ -5,10 +8,24 @@ interface Props {
 }
 
 const Header: React.FC<Props> = ({ changeTheme }) => {
+  const {title ,icon, colors: { text } } = useContext(ThemeContext)
 
   return(
     <HeaderContainer>
       <p>Where in the world?</p>
+      <div 
+      style={{
+        width: '7%',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent:'space-between',
+        cursor: 'pointer'
+        }}
+      onClick={changeTheme}
+      >
+        <FontAwesomeIcon color={text} icon={icon}/>
+        <p style={{textTransform: 'capitalize'}}>{title} mode</p>
+      </div>
     </HeaderContainer>
   )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 
 const HeaderContainer = styled.header`
-  padding: 10px 20px;
+  padding: 10px 50px;
   width: 100%;
-  height: 50px;
+  height: 60px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
   background-color: ${props => props.theme.colors.elements};
 
